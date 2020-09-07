bb2f1ece60d040ce21f256561b5a6cd7be5779b8
EunsilChoi92 <atheung1@naver.com>
2020-09-07 11:54:21 +0900

    200907 수업 / DefaultServlet 설정

---
+++ MatZip/src/com/koreait/matzip/StaticContainer.java
@@ -0,0 +1,9 @@
+
+package com.koreait.matzip;
+
+import org.apache.catalina.servlets.DefaultServlet;
+
+public class StaticContainer extends DefaultServlet {}
+
+
+// res로 시작하는 주소값은 staticServlet이 잡는다

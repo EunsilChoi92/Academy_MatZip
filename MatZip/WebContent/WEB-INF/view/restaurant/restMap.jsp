171a5d5570f8adeb7bad61e35f49a2e48117d358
Administrator <Administrator@DESKTOP-ISVFPRU>
2020-09-11 17:50:15 +0900

    200911 수업 / 주소 좌표, 공통 코드 가져오기, 지도에 마커 찍기 작업중

--- MatZip/WebContent/WEB-INF/view/restaurant/restMap.jsp
+++ MatZip/WebContent/WEB-INF/view/restaurant/restMap.jsp
@@ -3,12 +3,34 @@
 <div id="sectionContainerCenter">
 	<div id="mapContainer" style="width:100%; height:100%;"></div>
 	<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=faf4b3a5a0e4ed4c610c95db44ba090d"></script>
+	<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 	<script>
 		const options = { //지도를 생성할 때 필요한 기본 옵션
 			center: new kakao.maps.LatLng(35.865824, 128.594012), //지도의 중심좌표.
-			level: 3 //지도의 레벨(확대, 축소 정도)
+			level: 5 //지도의 레벨(확대, 축소 정도)
 		};
 	
 		var map = new kakao.maps.Map(mapContainer, options); //지도 생성 및 객체 리턴
+		
+		console.log(map.getCenter());
+		
+		function getRestaurantList() {
+			axios.get('/restaurant/ajaxGetList').then(function(res){
+				console.log(res.data);
+				
+				res.data.forEach(function(item) {
+					const na = {
+							'Ga': item.lng,
+							'Ha': item.lat
+					}
+					console.log(`na : ${na}`);
+					const marker = new kakao.maps.Marker({
+						position: na
+					});
+					
+					marker.setMap(map);
+				})
+			});
+		}
 	</script>
 </div>
--- MatZip/src/com/koreait/matzip/restaurant/RestaurantController.java
+++ MatZip/src/com/koreait/matzip/restaurant/RestaurantController.java
@@ -2,14 +2,63 @@ package com.koreait.matzip.restaurant;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.koreait.matzip.CommonDAO;
+import com.koreait.matzip.CommonUtils;
 import com.koreait.matzip.Const;
+import com.koreait.matzip.SecurityUtils;
 import com.koreait.matzip.ViewRef;
+import com.koreait.matzip.db.RestaurantDAO;
+import com.koreait.matzip.vo.RestaurantVO;
 
 public class RestaurantController {
+	
+	private RestaurantService service;
+	
+	public RestaurantController() {
+		service = new RestaurantService();
+	}
+	
 	public String restMap(HttpServletRequest request) {
 		// 메뉴가 있는 템플릿, restMap.jsp가 화면에 열리도록
 		request.setAttribute(Const.TITLE, "식당");
 		request.setAttribute(Const.VIEW, "restaurant/restMap");
 		return ViewRef.TEMP_MENU_TEMP;
 	}
+	
+	public String restReg(HttpServletRequest request) {
+		final int I_M = 1;
+		request.setAttribute("categoryList", CommonDAO.selCodeList(I_M));
+		
+		request.setAttribute(Const.TITLE, "가게 등록");
+		request.setAttribute(Const.VIEW, "restaurant/restReg");
+		return ViewRef.TEMP_MENU_TEMP;
+	}
+	
+	public String restRegProc(HttpServletRequest request) {
+		String nm = request.getParameter("nm");
+		String addr = request.getParameter("addr");
+		double lat = CommonUtils.getDoubleParameter("lat", request);
+		double lng = CommonUtils.getDoubleParameter("lng", request);
+		int cd_category = CommonUtils.getIntParameter("cd_category", request);
+		int i_user = SecurityUtils.getLoginUser(request).getI_user();
+		
+		RestaurantVO param = new RestaurantVO();
+		
+		param.setNm(nm);
+		param.setAddr(addr);
+		param.setLat(lat);
+		param.setLng(lng);
+		param.setCd_category(cd_category);
+		param.setI_user(i_user);
+		
+		int result = service.restReg(param);
+		
+		return "redirect:/restaurant/restMap";
+		
+	}
+	
+	public String ajaxGetList(HttpServletRequest request) {
+		return "ajax:" + service.getRestList();
+	}
+	
 }
